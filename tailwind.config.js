b977a08356b66af38358519ba5edcf445ab2e4e6
Peterson <pmugasa78@gmail.com>
2022-10-26 20:00:40 +0200

    added navbar section

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
+    },
+    fontFamily:{
+      nunito: 'Nunito, sans-serif',
+    },
+    extend: {
+      colors: {
+        secondary: '#f0b000',
+        primary: '#f4f0ec',
+        txtColor: '#201e1d'
+      },
+    },
   },
   plugins: [],
-}
+};
